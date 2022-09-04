4e5c9aa356d052c7403162f1419558f935d49b04
mkowalewski <aspoka1@gmail.com>
2022-09-04 16:31:33 +0200

    Create test santa sleigh dummy implementation

---
+++ src/test/groovy/pl/mkowalewski/TestSantaSleigh.groovy
@@ -0,0 +1,17 @@
+package pl.mkowalewski
+
+class TestSantaSleigh implements SantasSleigh {
+
+    @Override
+    void pack(Present present) {
+
+    }
+
+    int countPresents() {
+        0
+    }
+
+    int getPresent(int integer) {
+        0
+    }
+}
