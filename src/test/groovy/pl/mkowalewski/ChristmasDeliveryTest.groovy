1b63a2ef121286113df165a7da4af0c088d8c978
mkowalewski <aspoka1@gmail.com>
2022-09-04 15:54:17 +0200

    Configure spock tests

---
+++ src/test/groovy/pl/mkowalewski/ChristmasDeliveryTest.groovy
@@ -0,0 +1,17 @@
+package pl.mkowalewski
+
+import spock.lang.Specification
+
+class ChristmasDeliveryTest extends Specification {
+
+    def "configuration test"() {
+        given:
+        var a;
+
+        when:
+        a = 1;
+        then:
+        a == 1
+    }
+
+}
